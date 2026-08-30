6585be94c581ccf9e7f6e3b629cfbe2d377ea9af
Allison <allisonsouza10261@gmail.com>
2026-08-29 21:48:34 -0300

    Add responsive config for the input|filter

--- src/components/InputSearchMovie.jsx
+++ src/components/InputSearchMovie.jsx
@@ -8,33 +8,41 @@ import LanguageFilter from "./LanguageFilter";
 function InputSearchMovie({ setTitle, setPageNumber, genres, setMovies }) {
   const [filterStatus, setFilterStatus] = useState(false);
 
+  //MAKE IT RESPONSIVE ...
+
   /*
   min-h-dvh = “a página nunca será menor que a tela”
   max-w-xl  = “o conteúdo não vai ficar largo demais no desktop”
   w-full    = “no celular, ocupe a largura disponível”  
   */
   return (
-    <div className="flex justify-center items-center gap-2 w-full">
-      <button
-        className="flex gap-2 p-3 rounded-full bg-slate-900 hover:cursor-pointer transition hover:bg-slate-600"
-        onClick={() => setFilterStatus(!filterStatus)}
-      >
-        <FilterIcon />
-        <h2>Filter</h2>
-      </button>
+    <div className="flex flex-col-reverse items-center w-64 gap-2 md:flex-row md:w-full md:justify-center">
+      <div className="flex gap-2">
+        <button
+          className="flex gap-2 p-3 rounded-full bg-slate-900 hover:cursor-pointer transition hover:bg-slate-600"
+          onClick={() => setFilterStatus(!filterStatus)}
+        >
+          <FilterIcon />
+          <h2>Filter</h2>
+        </button>
+
+        <button className="transition hover:opacity-50 hover:cursor-pointer">
+          <Search />
+        </button>
+      </div>
       {filterStatus ? (
-        <>
+        <div className="flex gap-2">
           <GenreFilter setMovies={setMovies} genres={genres} />
           <YearFilter setMovies={setMovies} />
           <CountryFilter setMovies={setMovies} />
           <LanguageFilter setMovies={setMovies} />
-        </>
+        </div>
       ) : (
         ""
       )}
 
       <input
-        className="w-3xl rounded-full  px-4 py-3 bg-slate-900 opacity-50 text-slate-50 outline-none  placeholder:text-slate-50 transition hover:shadow-xl hover:opacity-100 hover: focus:opacity-100"
+        className="w-80 sm:w-2xl md:w-3xl rounded-full  px-4 py-3 bg-slate-900 opacity-50 text-slate-50 outline-none  placeholder:text-slate-50 transition hover:shadow-xl hover:opacity-100 hover: focus:opacity-100"
         type="text"
         onChange={(event) => {
           setTitle(event.target.value);
@@ -42,9 +50,6 @@ function InputSearchMovie({ setTitle, setPageNumber, genres, setMovies }) {
         }}
         placeholder="Search through 300+ movies online"
       />
-      <button className="transition hover:opacity-50 hover:cursor-pointer">
-        <Search />
-      </button>
     </div>
   );
 }
